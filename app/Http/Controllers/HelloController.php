8c9d501b8ed410456d8744b38ace89a012435689
kentaq23 <k20ken02ta@icloud.com>
2022-06-17 13:50:18 +0900

    text P.72

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -23,24 +23,21 @@ function tag($tag, $txt) {
 class HelloController extends Controller
 {
   
-   public function index() {
-       global $head, $style, $body, $end;
-      
-       $html = $head . tag('title','Hello/Index') . $style . $body
-           . tag('h1','Index') . tag('p','this is Index page')
-           . '<a href="/hello/other">go to Other page</a>'
-           . $end;
-       return $html;
+   public function index()
+   {
+       $data = [
+           'msg'=>'お名前を入力下さい。',
+       ];
+       return view('hello.index', $data);
    }
 
-   public function other() {
-       global $head, $style, $body, $end;
-      
-       $html = $head . tag('title','Hello/Other') . $style . $body
-           . tag('h1','Other') . tag('p','this is Other page')
-           . $end;
-       return $html;
+   public function post(Request $request)
+   {
+       $msg = $request->msg;
+       $data = [
+           'msg'=>'こんにちは、' . $msg . 'さん！',
+       ];
+       return view('hello.index', $data);
    }
 
 }
-
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 //  Route::get('hello', 'HelloController@index');
 //  Route::get('hello/other', 'HelloController@other');
 
-Route::get('hello', function() {
-    return view('hello.index');
-});
+Route::get('hello', 'HelloController@index');
+
+Route::post('hello', 'HelloController@post');
 //--------実習用----------
