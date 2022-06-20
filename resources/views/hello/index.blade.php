04dd48a6cc845e89f6941f7521e48ea5f2077bd7
kentaq23 <k20ken02ta@icloud.com>
2022-06-20 15:02:53 +0900

    text P.89

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;foreachディレクティブの例</p>
-    <ol>
-    @foreach($data as $item)
-    <li>{{$item}}
-    @endforeach
-    </ol>
- </body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+   @parent
+   インデックスページ
+@endsection
+
+@section('content')
+   <p>ここが本文のコンテンツです。</p>
+   <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
+
