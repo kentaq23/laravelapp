64522b85163194bdb3568f2e0696011134f71dfb
kentaq23 <k20ken02ta@icloud.com>
2022-07-06 12:06:37 +0900

    P.251 スコープを組み合わせる

--- app/Person.php
+++ app/Person.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    public function getData()
-{
-   return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-}
+    public function scopeAgeGreaterThan($query, $n)
+    {
+       return $query->where('age','>=', $n);
+    }
+    
+    public function scopeAgeLessThan($query, $n)
+    {
+       return $query->where('age', '<=', $n);
+    }
 }
