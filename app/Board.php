b7bd99be679c436c094b43940e69168b8a088a64
kentaq23 <k20ken02ta@icloud.com>
2022-07-08 13:33:04 +0900

    P.285 関連レコーディングの有無

--- app/Board.php
+++ app/Board.php
@@ -6,16 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    protected $guarded = array('id');
-  
-    public static $rules = array(
-       'person_id' => 'required',
-       'title' => 'required',
-       'message' => 'required'
-   );
+    // 新たにメソッドを追加
+public function person()
+{
+   return $this->belongsTo('App\Person');
+}
 
-   public function getData()
-   {
-       return $this->id . ': ' . $this->title;
-   }
+// 既にあるメソッドを修正
+public function getData()
+{
+   return $this->id . ': ' . $this->title . ' (' 
+      . $this->person->name . ')';
+}
 }
